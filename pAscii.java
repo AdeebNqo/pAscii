67f9d177083e1f389545cc6236f7a9c15097602b
AdeebNqo <adeebnqo@gmail.com>
2013-03-02 21:52:24 +0200

    Updated mapping of pixel to character

--- pAscii.java
+++ pAscii.java
@@ -1,8 +1,10 @@
 import java.io.File;
 import java.awt.image.BufferedImage;
 import javax.imageio.ImageIO;
+import java.awt.Color;
 class pAscii{
 	String path;
+	
 	public pAscii(String path){
 		this.path = path;
 	}
@@ -17,12 +19,14 @@ class pAscii{
 			for (int i=0;i<h;i++){
 				for (int j=0;j<w;j++){
 					int pix = img.getRGB(j,i);
-					if (pix==-1){
-						pixels[i][j]=" ";
-					}
-					else{
-						pixels[i][j]="|";
-					}
+					Color color = new Color(pix);
+					int r = color.getRed();
+					int g = color.getGreen();
+					int b = color.getBlue();
+					int sum = r+g+b;
+					int total = 3*255;
+					float avg = sum/((float)total);
+					pixels[i][j] = ""+getChar(avg);
 				}
 			}
 			//printing array
@@ -35,5 +39,38 @@ class pAscii{
 		}catch(Exception e){
 			e.printStackTrace();
 		}
+	}
+	public char getChar(float Avg){
+		if (Avg <  0.1){
+			return '.';
+		}
+		else if(Avg <  0.2){
+			return ',';
+		}
+		else if (Avg <  0.3){
+			return ';';
+		}
+		else if (Avg <  0.4){
+			return '!';
+		}
+		else if (Avg <  0.5){
+			return 'v';
+		}
+		else if (Avg <  0.6){
+			return 'l';
+		}
+		else if (Avg <  0.7){
+			return 'L';
+		}
+		else if (Avg <  0.8){
+			return 'F';
+		}
+		else if (Avg <  0.9){
+			return 'E';
+		}
+		else if (Avg <= 1.0){
+			return '$';
+		}
+		return ' ';
 	}
 }
